642cfca93ad588f54ffa3eaff8a0b66e0bb9a1b3
Roman <rallyshops@gmail.com>
2022-03-30 14:56:01 +0300

    added background btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -92,12 +92,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
+  bg: string = 'https://images.unsplash.com/photo-1646877765097-8b3389ce95f2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY0ODYzOTc5Ng&ixlib=rb-1.2.1&q=80&w=1920'
+
   prepareRoute(outlet: RouterOutlet): any {
     if (outlet.isActivated) {
       return outlet.activatedRouteData['tab'];
-    }
-    
+    } 
   } 
+
+  changeBGImage() {
+    fetch(' https://source.unsplash.com/random', {
+      method: 'HEAD'
+    })
+  }
 
 
   
