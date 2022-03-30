7e9fc6a0056866e1768e23c0d838c70ed6012ef9
Roman <rallyshops@gmail.com>
2022-03-30 19:15:23 +0300

    added background img changer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -87,12 +87,36 @@ import { RouterOutlet } from '@angular/router';
         ])
         
       ])
+    ]),
+
+    trigger('bgAnim', [
+      transition(':leave', [
+        animate(1000, style({
+          opacity: 0
+        }))
+      ])
+    ]),
+
+    trigger('fadeAnim', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(250, style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate(250, style({ opacity: 0}))
+      ])
     ])
   ]
 })
 export class AppComponent {
 
-  bg: string = 'https://images.unsplash.com/photo-1646877765097-8b3389ce95f2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY0ODYzOTc5Ng&ixlib=rb-1.2.1&q=80&w=1920'
+  backgrounds: string[] = [
+    'https://images.unsplash.com/photo-1646877765097-8b3389ce95f2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY0ODYzOTc5Ng&ixlib=rb-1.2.1&q=80&w=1920'
+  ]
+
+  loadingBGImage!: boolean;
 
   prepareRoute(outlet: RouterOutlet): any {
     if (outlet.isActivated) {
@@ -100,12 +124,28 @@ export class AppComponent {
     } 
   } 
 
-  changeBGImage() {
-    fetch(' https://source.unsplash.com/random', {
+  async changeBGImage(): Promise<any> {
+    this.loadingBGImage = true;
+    const result = await fetch(' https://source.unsplash.com/random/1920x1080', {
       method: 'HEAD'
     })
+    
+    const alreadyGot = this.backgrounds.includes(result.url);
+
+    if (alreadyGot) {
+      return this.changeBGImage();
+    }
+
+    this.backgrounds.push(result.url);
   }
 
+  onBGImageLoad(imgEvent: Event) {
+    const imgElement = imgEvent.target as HTMLImageElement;
+    const src = imgElement.src
+    this.backgrounds = this.backgrounds.filter(b => b === src);
+      
+    this.loadingBGImage = false;
+  }
 
   
 }
